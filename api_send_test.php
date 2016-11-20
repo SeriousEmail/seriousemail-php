89c72715b90fc1cad36ba2d1ef6c257da0183ab2
Kurt <kurt@dommermuth.com>
2016-11-20 09:23:29 -0500

    First commit

---
+++ api_send_test.php
@@ -0,0 +1,47 @@
+<?php
+
+
+require_once('lib/SeriousEmail.php');
+
+$api_secret = 'your_api_secret';
+$se = new SeriousEmail($api_secret);
+
+$data = array(
+		'public_api_id' => 'your_public_api_key', 
+		'campaign_id' => 89,
+		'template_id' => 442,
+		'recipient_info' => array( 
+		
+								array (
+								
+									'first_name' => 'Sam',
+									'last_name' => 'Lamb',
+									'email' => 'test1t@example.com',
+									'custom' => array(
+														'Points' => 92,
+														'Balance' => 500,
+													 )
+	
+								),
+								
+								array (
+								
+									'first_name' => 'Bob',
+									'last_name' => 'Smith',
+									'email' => 'test2t@example.com',
+									'custom' => array(
+														'Points' => 500,
+														'Balance' => 20,
+													 )
+								),
+							
+							),	
+							
+		
+	    );
+
+$send = $se->send($data, 1);  //0 = debugging false
+
+if(isset($send)){
+	echo $send->feedback;
+}
---
+++ lib/SeriousEmail.php
@@ -0,0 +1,104 @@
+<?php
+/**
+  * This library allows you to quickly and easily send emails through Serious.Email using PHP.
+  *
+  * PHP version 5.3
+  *
+  * @author    Kurt Dommermuth <kurt@serious.email>
+  * @copyright 2016 Serious Email
+  * @license   https://opensource.org/licenses/MIT The MIT License
+  * @version   GIT: <git_id>
+*/
+
+/**
+  * Interface to the Serious.Email Web API
+*/
+  
+class SeriousEmail
+{
+	private $version = '1.0.1';
+	
+	//secret can be retreived from your Serious.Email account - https://serious.email/account/settings#tab_api-settings
+	private $api_secret;
+	
+	//url for triggering remote sends
+	private $url = 'https://serious.email/send';
+
+	/**
+     * API keys (including $api_secret) issued for account holders at serious.email
+     *
+    */
+	public function __construct($api_secret)
+    {
+		$this->api_secret = $api_secret;
+    }
+		
+	/**
+     * Trigger an email
+     *
+     *  sends a custom Serious Email template
+     *  This requires authentication.
+     *  Request type: POST
+	 *  @param bool $debug will reveal additional information to help client debug.  Not for use in production environment.
+	 *  @param array $data is array of information required to send info.
+	 *  
+    */
+	public function send($data, $debug=0){
+		
+		//if debug = 1 additiona connection information and retrieved data will displayed
+		$data['debug'] = $debug;
+		
+		//convert $data to json
+		$data_string = json_encode($data);
+		
+		//create signature so we can validate that this is a legitimate request				
+		$signature = base64_encode(hash_hmac('sha256', $data_string , $this->api_secret, true));
+		
+		//establish headers for curl and add signature
+		$header = array(
+						"Accept: application/json",
+						"Content-Length: " . strlen($data_string),
+						"signature: " . $signature,
+						);
+		
+		//initialize curl
+		$ch = curl_init();
+		//set curl header + other curl options
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+		//add $data
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		//url establish as class var
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		//get a raw response for debugging
+		$raw_response = curl_exec($ch);	
+		
+		if($debug){
+			
+			$ee = curl_getinfo($ch);
+			echo "<strong>Raw curl response:</strong><br><br>";
+			print_r($raw_response);
+			echo "<br><br><strong>curl info:</strong><br><br>";
+			print_r($ee);
+			echo "<br><br>";
+		}
+			
+		$response = json_decode($raw_response);
+		
+
+		if(!isset($response)) {
+			
+			$response = array(
+					'success' => 0,
+					'feedback' => 'Message failed to send.  Did not reach the server. Try turning on debugging for more info.',
+					);
+					
+			$response = json_decode(json_encode($response));		
+		}
+		
+		return $response;
+	}
+}
