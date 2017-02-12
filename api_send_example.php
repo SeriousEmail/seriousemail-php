440ab4521bef0a99c715b076faccde7298f667e4
Kurt <kurt@dommermuth.com>
2017-02-12 16:10:26 -0500

    updated

--- api_send_example.php
+++ api_send_example.php
@@ -1,24 +1,13 @@
 <?php
 
-<<<<<<< HEAD
-
-require_once('lib/SeriousEmail.php');
-
-$api_secret = 'your_api_secret';
-$se = new SeriousEmail($api_secret);
-
-$data = array(
-		'public_api_id' => 'your_public_api_key', 
-=======
-//require 'vendor/autoload.php'; //USE THIS IF USING COMPOSER
-require_once('src/SeriousEmail/SeriousEmail.php');  //Update this path if you changed the location of SeriousEmail.php
+require 'vendor/autoload.php'; //USE THIS IF USING COMPOSER
+//require_once('src/SeriousEmail/SeriousEmail.php');  //Update this path if you changed the location of SeriousEmail.php
 
 $api_secret = 'YOUR_API_SECRET';
 $se = new SeriousEmail($api_secret);
 
 $data = array(
 		'public_api_id' => 'YOUR_PUBLIC_API_KEY', 
->>>>>>> 8606751e0dd07053902a6d1c87a08383da81e887
 		'campaign_id' => 89,
 		'template_id' => 442,
 		'recipient_info' => array( 
@@ -27,11 +16,8 @@ $data = array(
 								
 									'first_name' => 'Sam',
 									'last_name' => 'Lamb',
-<<<<<<< HEAD
 									'email' => 'test1t@example.com',
-=======
 									'email' => 'success@simulator.amazonses.com',
->>>>>>> 8606751e0dd07053902a6d1c87a08383da81e887
 									'custom' => array(
 														'Points' => 92,
 														'Balance' => 500,
@@ -43,11 +29,8 @@ $data = array(
 								
 									'first_name' => 'Bob',
 									'last_name' => 'Smith',
-<<<<<<< HEAD
 									'email' => 'test2t@example.com',
-=======
 									'email' => 'success+label1@simulator.amazonses.com',
->>>>>>> 8606751e0dd07053902a6d1c87a08383da81e887
 									'custom' => array(
 														'Points' => 500,
 														'Balance' => 20,
@@ -63,8 +46,6 @@ $send = $se->send($data, 1);  //0 = debugging false
 
 if(isset($send)){
 	echo $send->feedback;
-<<<<<<< HEAD
-=======
 	echo "<br>send_time: " . $send->send_time->date;
 	echo "<br>fail_count: " . $send->fail_count;
 	echo "<br>success_count: " . $send->success_count;
@@ -89,7 +70,6 @@ if(isset($send)){
 	$errors = $send->errors;
 	if(count($errors)){
 		echo "<br><strong>Errors</strong>";
-
 		foreach($errors as $error){		
 			echo "<br>error: " . $error;
 		}
@@ -102,5 +82,4 @@ if(!empty( $send->curl_info )){
 	print "<pre>";
 	print_r($send->curl_info);
 	print "</pre>";
->>>>>>> 8606751e0dd07053902a6d1c87a08383da81e887
 }
